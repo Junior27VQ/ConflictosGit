c22480515016c84be6b4312df1659110c17d86c9
Junior27VQ <favq1999@outlook.com>
2026-03-21 23:48:05 -0500

    Modificar atributo de biletera

---
+++ ControlDeVerciones/src/conflictos/Main.java
@@ -0,0 +1,16 @@
+package conflictos;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Billetera miBilletera=new Billetera(100.0, "Juan Perez");
+		miBilletera.mostrarSaldo();
+		miBilletera.transferir(20.0);
+		miBilletera.transferir(25.0);
+		miBilletera.transferir(30.0);
+		miBilletera.mostrarSaldo();
+
+	}
+}
