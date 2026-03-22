5c6076c85aef2504e3f6f26437012a1d624f6a68
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-03-21 23:27:26 -0500

    main modificado

--- ControlDeVerciones/src/conflictos/Main.java
+++ ControlDeVerciones/src/conflictos/Main.java
@@ -1,13 +1,23 @@
 package conflictos;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Billetera miBilletera=new Billetera(100.0, "Juan Perez");
-		miBilletera.mostrarSaldo();
-		miBilletera.transferir(20.0);
-		miBilletera.mostrarSaldo();
+
+Scanner scanner = new Scanner(System.in);
+
+        Billetera miBilletera = new Billetera(100.0, "Juan Perez");
+        miBilletera.mostrarSaldo();
+
+        System.out.print("¿Cuánto desea transferir? ");
+        double monto = scanner.nextDouble();
+
+        miBilletera.transferir(monto);
+        miBilletera.mostrarSaldo();
+
+        scanner.close();
+    }
 
 	}
-
-}
