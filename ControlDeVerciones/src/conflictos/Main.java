69c04896159ba2bd41a74d2a32ddf826f4d340d9
Junior27VQ <favq1999@outlook.com>
2026-03-21 23:51:42 -0500

    unificacion

--- ControlDeVerciones/src/conflictos/Main.java
+++ ControlDeVerciones/src/conflictos/Main.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		Billetera miBilletera=new Billetera(100.0, "Juan Perez");
 		miBilletera.mostrarSaldo();
+		System.out.print("¿Cuánto desea transferir? ");
+        double monto = scanner.nextDouble();
+
 		miBilletera.transferir(20.0);
 		miBilletera.transferir(25.0);
 		miBilletera.transferir(30.0);
 		miBilletera.mostrarSaldo();
+
+		 scanner.close();
 
 	}
 }
